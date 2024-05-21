6d6f7763e876540f1dd9da4373d41cdbfd373bed
Elia04Ita <007titanluna@gmail.com>
2024-05-21 20:00:20 +0200

    %current_datetime%

--- gitload.bat
+++ gitload.bat
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-current_datetime=$(date '+%d-%m-%Y %H:%M')
+rem Delete the "obj" folder
+rd /s /q obj
+
+rem Delete the "bin" folder
+rd /s /q bin
+
+del "ECommerce.sln"
 
 git add .
 
-git commit -am "Versione del $current_datetime"
+git commit -am "%current_datetime%"
 
 git push
 
-rm "ECommerce.sln"
+del "ECommerce.sln"
